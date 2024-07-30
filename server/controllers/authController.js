2260a80ba08a9bb7d9e5024d3bcdb035e875e6c7
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-07-30 03:48:29 -0700

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,11 +3,18 @@ const jwt=require('jsonwebtoken')
 const catchAsync=require('../utils/catchAsync')
 const bcrypt=require('bcrypt')
 
+const nodemailer = require('nodemailer');
 
 const signToken=(id)=>{
     return jwt.sign({id:id},process.env.JWT_SECRET)
 }
-
+let transporter = nodemailer.createTransport({
+    service: 'gmail', // Example using Gmail
+    auth: {
+        user: "tirumelk@gmail.com",
+        pass: "oaol elkh mzfq htnv"
+    }
+});
 exports.signup=catchAsync(async (req,res,next)=>{
     const {email,password}=req.body
     const newUser=await Users.create({
@@ -51,3 +58,32 @@ exports.login=async (req,res,next)=>{
     }
     
 }
+exports.contactUs=catchAsync(async (req,res)=>{
+  let mailOptions = {
+    from: req.body.email,
+    to: "leulmelkamu15@gmail.com", // List of recipients
+    subject: 'Reset Your Password', // Subject line
+    text: 'Greetings from the team.', // Plain text body
+    html: `
+        <p>Email from ${req.body.firstName} ${req.body.lastName}</p>
+        you can contact the user with ${req.body.phoneNumber}
+        <p>
+          ${req.body.description}
+        </p>
+        
+        <p>Thanks</p>
+    `
+};
+    transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        res.status(400).json({
+            status:400,
+            message:"An Error Occurred"})
+        console.log(error)
+    }
+    console.log('Email sent successfully:', info.response);
+    res.status(200).json({status:200,
+        message:"Email sent successfully, Please check your email to verify and please check the spams folder in your email if you don't find it in your inbox"})
+    });
+})   
+   

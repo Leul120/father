005ed5c6aab65162b28afa823a66c87c7f885508
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-11-19 14:17:27 -0800

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -98,7 +98,7 @@ exports.protect=async(req,res,next)=>{
     let token;
     if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
         token=req.headers.authorization.split(' ')[1]
-        console.log(token)
+        // console.log(token)
     }
 
     if(!token){

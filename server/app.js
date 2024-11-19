c672c448aecce7e97dbdf4a80f5e6a9238d96791
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-11-19 05:34:37 -0800

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,10 +2,19 @@ const express=require('express')
 const app=express()
 const userRoute=require('./routes/userRoute')
 const cors=require('cors')
+const path=require('path')
 const connectDB=require('./db/connectDB')
 require('dotenv').config()
 app.use(express.json())
+// if (process.env.NODE_ENV === 'production') {
+//   // Serve static files from the React build directory
+//   app.use(express.static(path.join(__dirname, '../client/public')));
 
+//   // Handle React routing, return all requests to React app
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../client/public', 'index.html'));
+//   });
+// }
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],

c24c56b2b0c5f343d34a8ce9fa2ca3b8b7c53f46
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-11-20 05:49:54 -0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -38,8 +38,7 @@ function App() {
           <Route path='/experiences' element={<ExperienceForm/>}/>
           <Route path='/languages' element={<LanguageForm/>}/>
           <Route path='/skills' element={<SkillForm/>}/>
-          {/* <Route path='/signup' element={<Signup/>}/> */}
-          <Route path='login' element={<Login/>}/>
+          <Route path='/login' element={<Login/>}/>
           <Route path='/publications' element={<PublicationForm/>}/>
           <Route path='/user-profile' element={<UserProfileForm/>}/>
           <Route path="/contact-us" element={<ContactUs/>}/>

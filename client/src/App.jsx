18361320a09e8435c5c2c1e592abf3d45753b453
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-11-29 12:16:10 -0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -42,7 +42,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/publications" element={<PublicationForm />} />
           <Route path="/user-profile" element={<UserProfileForm />} />
-          <Route path="/contact-us" element={<ContactUs />} />
+          <Route path="/contact" element={<ContactUs />} />
         </Routes>
       </Router>
     </AppContext.Provider>
